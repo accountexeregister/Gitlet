e405542be60b6b26d66f2ab6dcd6cef396016346
accountexeregister <yeohchongyi2@outlook.com>
2022-12-28 01:11:06 +0800

    Remove deleted Gitlet directory

--- Gitlet/src/FileComparator.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-import java.util.Comparator;
-
-public class FileComparator implements Comparator<File> {
-
-    public int compare(File o1, File o2) {
-        return o1.getName().compareTo(o2.getName());
-    }
-}
